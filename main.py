82a3a9ff210a7aa0b72cdf7055f3e0098fb90571
AmieRamie <ramesh.amr07@gmail.com>
2024-11-25 10:55:29 -0500

    updating reset_tables endpoint to allow for full update of ganache network and dynamoDB entries

--- main.py
+++ main.py
@@ -44,6 +44,8 @@ async def reset_tables():
     try:
         ds.clear_tables()
         response = ds.restart_ganache()
+        with open('data.json', 'w') as json_file:
+            json.dump({}, json_file, indent=4)
         return response
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
